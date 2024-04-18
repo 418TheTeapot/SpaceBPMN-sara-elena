ccd6e126baa44910c948267e5d507c509876ea59
Elena Ricci <elena.ricci@studenti.unicam.it>
2024-04-18 16:55:13 +0200

    pulizia

--- example/olc-js-properties-panel/provider/properties/PlaceProps.js
+++ example/olc-js-properties-panel/provider/properties/PlaceProps.js
@@ -7,15 +7,11 @@ export default function PlaceProps(element) {
     const properties = [];
 
     if (is(element, 'space:Place')) {
-        const propertyValue = element.businessObject.property || '';
-        const isOffValue = propertyValue === 'off';
 
         properties.push({
             id: 'placeProperties',
             element,
             component: Property,
-            propertyValue,
-            isOffValue,
             isEdited: isTextFieldEntryEdited,
         });
     }
